8cf4d9b444d865cfe69f705fd31c063a55d5ff3c
stynon <73358457+stynon@users.noreply.github.com>
2021-02-17 10:49:29 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pycom
+import time
+from machine import UART
+
+uart = UART(1)
+uart.init(9600, bits=8, parity=None, stop=1, timeout_chars=100, pins=('P3', 'P4'))
+
+while True:
+    header_bytes = uart.read(1)
+    while(header_bytes != b'\xff'):
+        header_bytes=uart.read(1)
+    data_high = int(uart.read(1)[0])
+    data_low = int(uart.read(1)[0])
+    data_sum = int(uart.read(1)[0])
+    sum = data_high + data_low
+    if (sum == data_sum + 1):
+        distance = data_high*256 + data_low
+        print(distance)
